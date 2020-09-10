673b9e55a626c8dddf1b29fb7e5d40007acef4fe
comicsansdan <70924068+comicsansdan@users.noreply.github.com>
2020-09-10 11:19:03 -0500

    Added folder to GitHub repository

---
+++ experiments/drawing-experiments/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Template p5 project
+Pippin Barr
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  background(255, 90, 175);
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
