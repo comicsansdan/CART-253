528630e0b5d67e6848fa3223096b415b5edb66e6
comicsansdan <70924068+comicsansdan@users.noreply.github.com>
2020-09-10 11:50:37 -0500

    Messing around with drawing shapes

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -9,7 +9,20 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
-  background(255, 90, 175);
+  createCanvas(500, 500);
+
+  background(200, 200, 200);
+
+  point(50, 50);
+
+  rectMode(CENTER);
+  rect(250, 250, 250, 250);
+
+  ellipse(250, 250, 150, 150);
+
+  line(250, 0, 250, 500);
+
+  line(0, 250, 500, 250);
 }
 
 // draw()
