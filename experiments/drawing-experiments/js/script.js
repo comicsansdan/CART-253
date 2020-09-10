6363d30ffa246f6cf9fb026aee8e26f9bfd6b6cb
comicsansdan <70924068+comicsansdan@users.noreply.github.com>
2020-09-10 15:47:04 -0500

    Drawing experiment: added colors

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -13,15 +13,19 @@ function setup() {
 
   background(200, 200, 200);
 
-  point(50, 50);
+  noStroke();
 
   rectMode(CENTER);
+  fill( 239, 150, 89,);
   rect(250, 250, 250, 250);
 
+  fill( 39, 230, 180, 150);
   ellipse(250, 250, 150, 150);
 
+  stroke(255, 0, 0);
   line(250, 0, 250, 500);
 
+  stroke(0, 255, 0);
   line(0, 250, 500, 250);
 }
 
