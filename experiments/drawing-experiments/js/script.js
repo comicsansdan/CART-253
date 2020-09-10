23070e7327680ca3b8dc98adee94879f8cd2aa71
comicsansdan <70924068+comicsansdan@users.noreply.github.com>
2020-09-10 16:30:22 -0500

    Added a proper title and description to the script

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -1,8 +1,8 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Drawing Experiments
+Daniel Cacatian
 
-Here is a description of this template p5 project.
+Experimenting with p5's drawing and color functions.
 **************************************************/
 
 // setup()
@@ -11,17 +11,21 @@ Here is a description of this template p5 project.
 function setup() {
   createCanvas(500, 500);
 
+  // Background color
   background(200, 200, 200);
 
   noStroke();
 
+  // Random rectangle
   rectMode(CENTER);
   fill( 239, 150, 89,);
   rect(250, 250, 250, 250);
 
+  // Circle in the center
   fill( 39, 230, 180, 150);
   ellipse(250, 250, 150, 150);
 
+  // Crosshairs +
   stroke(255, 0, 0);
   line(250, 0, 250, 500);
 
