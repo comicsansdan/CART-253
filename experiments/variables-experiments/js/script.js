c567507ebb4c9592092af441e1af547f09d135d6
comicsansdan <70924068+comicsansdan@users.noreply.github.com>
2020-09-16 11:12:29 -0500

    Learned about built-in variables

--- experiments/variables-experiments/js/script.js
+++ experiments/variables-experiments/js/script.js
@@ -9,33 +9,18 @@ Experimenting with p5's drawing and color functions.
 //
 // Description of setup() goes here.
 function setup() {
-  createCanvas(500, 500);
 
-  // Background color
-  background(200, 200, 200);
+createCanvas(windowWidth, windowHeight);
 
-  noStroke();
-
-  // Random rectangle
-  rectMode(CENTER);
-  fill( 239, 150, 89,);
-  rect(250, 250, 250, 250);
-
-  // Circle in the center
-  fill( 39, 230, 180, 150);
-  ellipse(250, 250, 150, 150);
-
-  // Crosshairs +
-  stroke(255, 0, 0);
-  line(250, 0, 250, 500);
-
-  stroke(0, 255, 0);
-  line(0, 250, 500, 250);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+
+background(mouseX, mouseY, 255);
+rectMode(CENTER);
+rect(width/2, height/2, 100, 100);
 
 }
