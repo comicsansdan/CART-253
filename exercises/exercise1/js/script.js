0196af962a3250b2808cdfc911480216a18e63d2
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-09-17 20:12:18 -0500

    Exercise 01: Added variables and canvas
    Created the canvas and added variables to be used in the setup()

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -5,10 +5,40 @@ Daniel Cacatian
 This activity consist of creating moving objects using variables, mapping, constrains and more!
 **************************************************/
 
+// VARIABLES
+
+let bg = {
+  r: 0,
+  g: 0,
+  b: 0,
+};
+
+let circle = {
+  x: 150,
+  size: 400,
+  fill: 255,
+  speed: 0.5,
+};
+
+let square = {
+  x: 450,
+  y: 450,
+  size: 200,
+  growthRate: 1,
+  fill: 255,
+}
+
+let triangle = {
+  size: 200,
+  fill: 255,
+}
+
 // setup()
 //
 // Setup that contains code for the canvas.
 function setup() {
+
+  createCanvas(900, 750);
 
 }
 
