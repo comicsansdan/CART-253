fb788eb80ccfb6dee143777a1f8a0cb4d51abc78
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-09-24 15:33:28 -0500

    Learned about loops

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -27,45 +27,73 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
   background(backgroundShade)
+  //
+  //   circle.x += circle.speed;
+  //   fill(255,255,255);
+  //
+  // //CONDITIONALS (ONLY 1 CONDITION)////////////////////////////////////////////////////
+  //   if (circle.x > width){
+  //     circle.speed = -circle.speed;
+  //   }
+  //
+  //   if (circle.x < 0){
+  //     circle.speed = -circle.speed;
+  //   }
+  //
+  //   if (mouseY < height/3){
+  //     fill(255,0,0);
+  //   }
+  //   else if (mouseY < 2 * height/3) {
+  //     fill(0,255,0);
+  //   }
+  //   else{
+  //     fill(0,0,255);
+  //   }
+  //
+  //   //CONDITIONALS (WITH MULTIPLE CONDITIONS)////////////////////////////////////////////////////
+  //   if (!(circle.x < width/3)) {
+  //       fill(200, 40, 180);
+  //   }
+  //
+  //
+  //   ellipse(circle.x, circle.y, circle.size);*/
+  //
+  //   //BOOLEANS////////////////////////////////////////////////////
+  //   if (mouseIsPressed) {
+  //     displayCircle = true;
+  //   }
+  //
+  //   if (displayCircle) {
+  //     ellipse(250,250,100,100);
+  //   }
+  //
+  //   //LOOPS////////////////////////////////////////////////////
 
-  /*circle.x += circle.speed;
-  fill(255,255,255);
+  noStroke();
+  fill(100, 200, 100);
 
-//CONDITIONALS (ONLY 1 CONDITION)////////////////////////////////////////////////////
-  if (circle.x > width){
-    circle.speed = -circle.speed;
+    let caterpillar = {
+      x:100,
+      y:250,
+      segmentSize: 50,
+    }
+  //
+  //   let x = caterpillar.x;
+  //   let numSegments = 8;
+  //   let segmentsDrawn = 0;
+  //
+  //   while (segmentsDrawn < numSegments) {
+  //     ellipse(x,caterpillar.y,caterpillar.segmentSize);
+  //     x += 40;
+  //     segmentsDrawn ++;
+  //   }*/
+
+  let x = caterpillar.x
+  let numSegments = 10;
+
+  for (let i = 0; i < numSegments; i++) {
+    ellipse(x, caterpillar.y, caterpillar.segmentSize);
+    x += 40;
   }
-
-  if (circle.x < 0){
-    circle.speed = -circle.speed;
-  }
-
-  if (mouseY < height/3){
-    fill(255,0,0);
-  }
-  else if (mouseY < 2 * height/3) {
-    fill(0,255,0);
-  }
-  else{
-    fill(0,0,255);
-  }
-
-  //CONDITIONALS (WITH MULTIPLE CONDITIONS)////////////////////////////////////////////////////
-  if (!(circle.x < width/3)) {
-      fill(200, 40, 180);
-  }
-
-
-  ellipse(circle.x, circle.y, circle.size);*/
-
-  //BOOLEANS////////////////////////////////////////////////////
-  if (mouseIsPressed) {
-    displayCircle = true;
-  }
-
-  if (displayCircle) {
-    ellipse(250,250,100,100);
-  }
-
 
 }
