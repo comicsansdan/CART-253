d265d72281b40a0f9ccf784a6596c5e148a55c82
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-09-24 17:49:15 -0500

    A4: Completed dodging covid-19 simulator

--- activities/04-dodging-covid-19/js/script.js
+++ activities/04-dodging-covid-19/js/script.js
@@ -5,15 +5,39 @@ Daniel Cacatian
 This activity consist of creating a simulation where you need to dodge COVID-19.
 **************************************************/
 
+let covid19 = {
+  x: 0,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 50,
+  fill: {
+    r: 255,
+    g: 0,
+    b: 0,
+  }
+};
 
+let user = {
+  x: 250,
+  y: 250,
+  size: 100,
+  fill: 255,
+};
+
+let numStatic = 1000;
 
 // setup()
 //
-// Setup that contains code which forms canvas
+// Setup that contains code which creates the canvas and spawns the virus
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
-  createCanvas(500, 500);
+  covid19.y = random(0, height);
+  covid19.vx = covid19.speed;
 
+  noCursor();
 }
 
 // draw()
@@ -21,5 +45,41 @@ function setup() {
 // Draw() contains all the elements to make the simulation.
 function draw() {
 
+  background(0);
+
+  //Display static
+  for (let i = 0; i < numStatic; i++) {
+  let x = random(0,width);
+  let y = random(0, height);
+  stroke(255);
+  point(x,y);
+  }
+
+  //COVID-19 Movement
+  covid19.x += covid19.vx;
+  covid19.y += covid19.vy;
+
+  if (covid19.x > width) {
+    covid19.x = 0;
+    covid19.y = random(0, height);
+  };
+
+  //User Movement
+  user.x = mouseX;
+  user.y = mouseY
+
+  //Check for catching covid19
+  let d = dist(user.x, user.y, covid19.x, covid19.y);
+  if (d < covid19.size/2 + user.size/2) {
+    noLoop();
+  }
+
+  //Display COVID-19
+  fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
+  ellipse(covid19.x, covid19.y, covid19.size);
+
+  //Display User
+  fill(user.fill, user.fill, user.fill);
+  ellipse(user.x, user.y, user.size);
 
 }
