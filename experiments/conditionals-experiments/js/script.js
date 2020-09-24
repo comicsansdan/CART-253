613282d88d6829f0dada31f9322a3dbc688cad9f
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-09-24 16:27:38 -0500

    Learned about movement and acceleration

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -7,10 +7,15 @@ Experimenting with p5's conditionals code.
 
 let backgroundShade = 0;
 let circle = {
-  x: 0,
+  x: 250,
   y: 250,
   size: 100,
-  speed: 2,
+  vx: 0,
+  vy: 0,
+  ax: 0,
+  ay: 0,
+  acceleration: 0.25,
+  maxSpeed: 5,
 }
 
 let displayCircle = false;
@@ -27,6 +32,31 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
   background(backgroundShade)
+
+  //   //MOVEMENT////////////////////////////////////////////////////
+
+  if (mouseX < circle.x) {
+    circle.ax = -circle.acceleration;
+  } else {
+    circle.ax = circle.acceleration;
+  }
+
+  if (mouseY < circle.y) {
+    circle.ay = -circle.acceleration;
+  } else {
+    circle.ay = circle.acceleration;
+  }
+
+  circle.vx += circle.ax
+  circle.vx = constrain(circle.vx, -circle.maxSpeed, circle.maxSpeed);
+  circle.vy += circle.ay
+  circle.vy = constrain(circle.vy, -circle.maxSpeed, circle.maxSpeed);
+
+  circle.x += circle.vx;
+  circle.y += circle.vy;
+
+  ellipse(circle.x, circle.y, circle.size);
+
   //
   //   circle.x += circle.speed;
   //   fill(255,255,255);
@@ -96,22 +126,22 @@ function draw() {
   //   x += 40;
   // }
 
-  //   //MOUSE INPUT////////////////////////////////////////////////////
-
-
-  ellipse(circle.x,circle.y,circle.size);
-  background (bg.r, bg.g, bg.b);
-
+  //   //   //MOUSE INPUT////////////////////////////////////////////////////
+  //
+  //
+  //   ellipse(circle.x,circle.y,circle.size);
+  //   background (bg.r, bg.g, bg.b);
+  //
 }
-
-let bg = {
-  r:0,
-  g:0,
-  b:0,
-}
-
-function mouseWheel() {
-  bg.r = random(0,255);
-  bg.g = random(0,255);
-  bg.b = random(0,255);
-}
+//
+// let bg = {
+//   r:0,
+//   g:0,
+//   b:0,
+// }
+//
+// function mouseWheel() {
+//   bg.r = random(0,255);
+//   bg.g = random(0,255);
+//   bg.b = random(0,255);
+// }
