24ed0b40e38a42cba3ff835fc8210bce858684e9
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-09-24 14:04:19 -0500

    Started conditionals experiment

---
+++ experiments/conditionals-experiments/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Conditionals Experiments
+Daniel Cacatian
+
+Experimenting with p5's conditionals code.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
