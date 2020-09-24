d98122c0e1e60896ed8c4ccc3f1aa31e5c849a05
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-09-24 14:35:56 -0500

    Learned about conditionals

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -5,16 +5,54 @@ Daniel Cacatian
 Experimenting with p5's conditionals code.
 **************************************************/
 
+let backgroundShade = 0;
+let circle = {
+  x: 0,
+  y: 250,
+  size: 100,
+  speed: 2,
+}
+
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+  createCanvas(500, 500);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(backgroundShade)
 
+  circle.x += circle.speed;
+  fill(255,255,255);
+
+//CONDITIONALS (ONLY 1 CONDITION)
+  if (circle.x > width){
+    circle.speed = -circle.speed;
+  }
+
+  if (circle.x < 0){
+    circle.speed = -circle.speed;
+  }
+
+  /*if (mouseY < height/3){
+    fill(255,0,0);
+  }
+  else if (mouseY < 2 * height/3) {
+    fill(0,255,0);
+  }
+  else{
+    fill(0,0,255);
+  }*/
+
+  //CONDITIONALS (WITH MULTIPLE CONDITIONS)
+  if (!(circle.x < width/3)) {
+      fill(200, 40, 180);
+  }
+
+
+  ellipse(circle.x, circle.y, circle.size);
 }
