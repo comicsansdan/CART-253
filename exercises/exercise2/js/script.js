c4b38eae35dc3fb8e2d33ef8a21a8d5b6191b37d
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-09-24 23:06:05 -0500

    E2: Added the user (main character)
    The user is the object that the player controls with the mouse position

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -5,17 +5,17 @@ Daniel Cacatian
 This activity consist of creating a simulation where there are conditionals, interactivity, loops and an image!
 **************************************************/
 
-// VARIABLES
+// VARIABLES //////////////////////////////////////
 
 let bg = {
-  r:0,
-  g:0,
-  b:0,
+  r: 0,
+  g: 0,
+  b: 0,
 }
 
 let clown = {
-  x: 250,
-  y: 250,
+  x: 0,
+  y: 0,
   size: 100,
   image: undefined,
   alpha: 1,
@@ -23,17 +23,17 @@ let clown = {
   vy: 0,
   ax: 0,
   ay: 0,
-  acceleration: 0.25,
-  maxSpeed: 10,
-};
+  acceleration: 0.5,
+  maxSpeed: 5,
+}
 
 function preload() {
   clown.image = loadImage("assets/images/clown.png")
 }
 
-let user {
-  x:0,
-  y:0,
+let user = {
+  x: 1800,
+  y: 900,
   size: 50,
   vx: 0,
   vy: 0,
@@ -57,5 +57,35 @@ function setup() {
 // Draw() consists of all the shapes, background and animations.
 function draw() {
 
+  background(bg.r, bg.g, bg.b);
+
+  // USER //////////////////////////////////////
+
+  //User's mouvement controls (mouse on X axis)
+  if (mouseX < user.x){
+    user.ax = -user.acceleration;
+  } else {
+    user.ax = user.acceleration;
+  }
+
+  //User's mouvement controls (mouse on Y axis)
+  if (mouseY < user.y) {
+    user.ay = -user.acceleration;
+  } else {
+    user.ay = user.acceleration;
+  }
+
+  //User's acceleration constraint
+  user.vx += user.ax
+  user.vx = constrain(user.vx, -user.maxSpeed, user.maxSpeed);
+  user.vy += user.ay
+  user.vy = constrain(user.vy, -user.maxSpeed, user.maxSpeed);
+
+  //User's velocity controls
+  user.x += user.vx;
+  user.y += user.vy;
+
+  //User drawing form
+  ellipse(user.x,user.y,user.size);
 
 }
