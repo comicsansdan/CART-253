03cf94544b86ecd9d3a3fc65e05d3b8b5b8a50e8
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-09-24 22:16:17 -0500

    E2: Added variables and canvas set-up
    I've added all the variables I plan on using in this exercise. Some numbers may change throughout the exercise.

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -2,20 +2,53 @@
 Exercise 2
 Daniel Cacatian
 
-This activity consist of creating a simulation where there are "if" statements, interactivity and loops!
+This activity consist of creating a simulation where there are conditionals, interactivity, loops and an image!
 **************************************************/
 
 // VARIABLES
 
+let bg = {
+  r:0,
+  g:0,
+  b:0,
+}
+
+let clown = {
+  x: 250,
+  y: 250,
+  size: 100,
+  image: undefined,
+  alpha: 1,
+  vx: 0,
+  vy: 0,
+  ax: 0,
+  ay: 0,
+  acceleration: 0.25,
+  maxSpeed: 10,
+};
+
+function preload() {
+  clown.image = loadImage("assets/images/clown.png")
+}
+
+let user {
+  x:0,
+  y:0,
+  size: 50,
+  vx: 0,
+  vy: 0,
+  ax: 0,
+  ay: 0,
+  acceleration: 0.25,
+  maxSpeed: 5,
+}
 
 // setup()
 //
 // Setup that contains code for the canvas.
 function setup() {
 
-  createCanvas(900, 1000);
-
-  noStroke();
+  createCanvas(windowWidth, windowHeight);
 
 }
 
