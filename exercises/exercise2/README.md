2b571b29033668b9a407fcb90f1d11d37edc18f4
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-09-24 21:45:23 -0500

    E2: Started exercise 2

---
+++ exercises/exercise2/README.md
@@ -0,0 +1,3 @@
+# Exercise 02: Dodge-em
+
+This README file contains the link to [Exercise 02: Dodge-em](https://danielcacatian.github.io/CART-253/exercises/exercise2/)
---
+++ exercises/exercise2/js/script.js
@@ -0,0 +1,28 @@
+/**************************************************
+Exercise 2
+Daniel Cacatian
+
+This activity consist of creating a simulation where there are "if" statements, interactivity and loops!
+**************************************************/
+
+// VARIABLES
+
+
+// setup()
+//
+// Setup that contains code for the canvas.
+function setup() {
+
+  createCanvas(900, 1000);
+
+  noStroke();
+
+}
+
+// draw()
+//
+// Draw() consists of all the shapes, background and animations.
+function draw() {
+
+
+}
