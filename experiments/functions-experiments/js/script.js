220c99c7ec286026a9413603033fe07e9d3b67ff
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-10-02 15:34:16 -0500

    Started Functions experiments

---
+++ experiments/functions-experiments/js/script.js
@@ -0,0 +1,22 @@
+/**************************************************
+Functions Experiments
+Daniel Cacatian
+
+Experimenting with p5's functions.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+
+}
