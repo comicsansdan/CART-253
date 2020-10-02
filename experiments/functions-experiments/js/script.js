66f5ced45b0be5b87c3ad9ec26e309c6daf77776
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-10-02 16:44:05 -0500

    Learned about function parameters

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -27,36 +27,55 @@ function setup() {
 function draw() {
   background(0);
 
-  move();
-  wrap();
-  display();
+  // FUNCTIONS PARAMETERS////////////////////////////////////////////////
+  parrallels(100, 100, 5, 1, 100,1);
+  parrallels(50, 50, 10, 2, 20, 10);
+  parrallels(200, 200, 15, 7, 3, 20);
+  parrallels(23, 99, 20, 0.5, 300, 1);
+  //FUNCTIONS//////////////////////////////////////////////////////
+  // move();
+  // wrap();
+  // display();
 
 }
 
-// Functions can also be reffered as modular. Also reuse
-function move() {
-  circle.x += circle.vx;
-  circle.y += circle.vy;
-}
-
-function wrap() {
-  if (circle.x > width) {
-    reset();
+// FUNCTIONS PARAMETERS////////////////////////////////////////////////
+function parrallels(x, y, numLines, lineWidth, lineHeight, lineSpacing) {
+  //For loop
+  for (let i = 0; i < numLines; i++) {
+    noStroke();
+    fill(255);
+    rectMode(CENTER);
+    rect(x, y, lineWidth, lineHeight);
+    x += lineSpacing;
   }
 }
 
-function display() {
-  rectMode(CENTER);
-  rect(circle.x, circle.y, circle.size, circle.size);
-}
-
-function reset() {
-  circle.x = 0;
-  circle.vx += 2;
-  circle.vy += -0.25;
-  circle.size += 5;
-}
-
-function mousePressed() {
-  reset();
-}
+//FUNCTIONS//////////////////////////////////////////////////////
+// Functions can also be reffered as modular. Also reuse
+// function move() {
+//   circle.x += circle.vx;
+//   circle.y += circle.vy;
+// }
+//
+// function wrap() {
+//   if (circle.x > width) {
+//     reset();
+//   }
+// }
+//
+// function display() {
+//   rectMode(CENTER);
+//   rect(circle.x, circle.y, circle.size, circle.size);
+// }
+//
+// function reset() {
+//   circle.x = 0;
+//   circle.vx += 2;
+//   circle.vy += -0.25;
+//   circle.size += 5;
+// }
+//
+// function mousePressed() {
+//   reset();
+// }
