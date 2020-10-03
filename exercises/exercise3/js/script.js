5647ad8f22a61cb88b2e920c48e5d3fdae9e1037
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-10-03 17:23:16 -0500

    E3: Added variables
    Added the variables I'll most likely be using. Their values will most likely change throughout the exercise

--- exercises/exercise3/js/script.js
+++ exercises/exercise3/js/script.js
@@ -5,6 +5,40 @@ Daniel Cacatian
 This exercise is to recreate the "Looking for Love" activity by using our own if statements and states
 **************************************************/
 
+// VARIABLES ////////////////////////////////////////////////
+let you = {
+  x: 0,
+  y: 0,
+  size: 50,
+  vx: 0,
+  vy: 0,
+  speed: 1,
+}
+
+let love = {
+  x: 0,
+  y: 0,
+  size: 50,
+  vx: 0,
+  vy: 0,
+  speed: 1,
+}
+
+let others = {
+  x: 0,
+  y: 0,
+  size: 50,
+  vx: 0,
+  vy: 0,
+  speed: 2,
+}
+
+let fakes = {
+  x: 0,
+  y: 0,
+  size: 50,
+}
+
 // setup()
 //
 // Description of setup() goes here.
