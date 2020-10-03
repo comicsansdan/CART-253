0e35d34478b88bc62517949e67595bdd6221f3cd
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-10-03 17:02:54 -0500

    E3: Started exercise 3 Love, actually

---
+++ exercises/exercise3/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Exercise 3: Love, actually
+Daniel Cacatian
+
+This exercise is to recreate the "Looking for Love" activity by using our own if statements and states
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
