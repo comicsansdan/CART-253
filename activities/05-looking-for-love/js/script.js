7ca8c834026303aa6638a0accf2473e5d5e07fa6
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-10-03 14:51:41 -0500

    A5: Started activity

---
+++ activities/05-looking-for-love/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Activity 05: Looking for Love
+Daniel Cacatian
+
+This activity consist of creating a small simulation with states and functions
+**************************************************/
+
+// setup()
+//
+// Setup
+function setup() {
+
+}
+
+// draw()
+//
+// Draw() does nothing.
+function draw() {
+
+}
