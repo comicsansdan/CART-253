5297c3f9c4fd3198c389fdd840c129fd97b477da
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-10-03 19:36:42 -0500

    E3: Added the soulmate
    Displayed the soulmate and gave her a linear movement. Will be used to create an ending

--- exercises/exercise3/js/script.js
+++ exercises/exercise3/js/script.js
@@ -13,12 +13,11 @@ let you = {
   speed: 1,
 }
 
-let love = {
+let soulmate = {
   x: 0,
   y: 0,
   size: 50,
-  vx: 0,
-  vy: 0,
+  vx: 1,
   speed: 1,
 }
 
@@ -41,13 +40,23 @@ let fakes = {
 //
 // Description of setup() goes here.
 function setup() {
+
   createCanvas(500, windowHeight)
   noStroke();
+  circleSetup();
 
-//Player's starting position
-  you.x = width / 2;
-  you.y = height / 8 * 7.25;
+}
 
+// CIRCLE SETUP ///////////////////////////////////////////////////////////////
+//Starting positions for the player, soulmate, others, etc.
+function circleSetup(){
+  //Player's starting position
+    you.x = width / 2;
+    you.y = height / 8 * 7.25;
+
+  //Soulmate's starting position
+    soulmate.x = width/ 2;
+    soulmate.y = height / 8;
 }
 
 // DRAW ///////////////////////////////////////////////////////////////////////
@@ -58,18 +67,39 @@ function draw() {
   background(255, 248, 255);
 
   display();
+  movement();
   controls();
 
 }
 
 // DISPLAY /////////////////////////////////////////////////////////////////////
-//Displays the lover, the player, others, etc.
+//Displays the player, their soulmate, others, etc.
 function display() {
   //You (the player)
-
   fill(118, 202, 228);
   ellipse(you.x, you.y, you.size);
+
+  //Soulmate
+  fill(255, 194, 202);
+  ellipse(soulmate.x, soulmate.y, soulmate.size);
+
 }
+
+// MOVEMENT /////////////////////////////////////////////////////////////////////
+//This function contains the movement of certain circles
+function movement(){
+  // Soulmate's movement
+  soulmate.x += soulmate.vx;
+
+  if (soulmate.x > width/4*3.5){
+    soulmate.vx += -soulmate.speed;
+  } else if (soulmate.x < width/7){
+    soulmate.vx += soulmate.speed;
+  }
+
+
+}
+
 
 // PLAYERS CONTROLS ///////////////////////////////////////////////////////////
 //Allows the player to control the circle with either the arrow keys or WASD
