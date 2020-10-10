c8931e994326d87b11cbf46012f3f3ea25511927
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-10-10 01:47:47 -0500

    P1: Added images
    I added images to give the simulation a more lively environment. Also, so it wasn't too plain and to help guide the player!

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -6,43 +6,72 @@ Here is a description of this template p5 project.
 **************************************************/
 
 let player = {
-  x: undefined,
-  y: undefined,
-  size: 50,
+  x: 0,
+  y: 0,
   speed: 2,
   image: undefined,
 }
 
 let sun = {
-  x: undefined,
-  y: undefined,
+  x: 0,
+  y: 0,
   size: 1000,
   alpha: 0.5,
   speed: 2,
 }
 
 let inventory = {
-  x: undefined,
-  y: undefined,
-  size: undefined,
+  x: 100,
+  y: 25,
+  width: 500,
+  height: 150,
+  image: undefined,
+  imageX: 50,
+  imageY: 50,
+  imageSize: 100,
 }
 
 let tree = {
-  x: undefined,
-  y: undefined,
+  x: 0,
+  y: 0,
   width: 150,
   height: 2000,
 }
 
 let stump = {
-  x: undefined,
-  y: undefined,
+  x: 0,
+  y: 0,
   width: 150,
-  height: 200,
+  height: 250,
+}
+
+let floor = {
+  x: 0,
+  y: 0,
+  width: undefined,
+  height: 100,
+}
+
+let tent = {
+  x: 0,
+  y: 0,
+  size: 600,
+  image: undefined,
+}
+
+let axe = {
+  x: 0,
+  y: 0,
+  image: `assets/images/axe.png`,
+  size: 100,
 }
 
 function preload(){
-
+  tent.image = loadImage(`assets/images/tent.png`);
+  player.image = loadImage(`assets/images/player_R.png`);
+  inventory.image = loadImage(`assets/images/inventory.png`);
+  axe.image = loadImage(`assets/images/axe.png`);
+  loadImage(`assets/images/player_L.png`);
 };
 
 
@@ -62,7 +91,7 @@ function setup() {
 function objectSetup(){
   // Player starting position
     player.x = width/2;
-    player.y = height/2*2;
+    player.y = height/2*1.65;
 
   // Tree starting position
     tree.x = width/6*5.5;
@@ -70,11 +99,24 @@ function objectSetup(){
 
   // Stump starting position
     stump.x = width/10;
-    stump.y = height/6*6;
+    stump.y = height;
 
   // Sun starting postion
     sun.x = width/2;
     sun.y = height;
+
+  // Floor starting postion
+    floor.x = width/2;
+    floor.y = height;
+
+  // Tent starting postion
+    tent.x = width/2;
+    tent.y = height/2*1.65;
+
+  // Tent starting postion
+    axe.x = width/10 + 60;
+    axe.y = height - 190;
+
 }
 
 // draw()
@@ -92,18 +134,17 @@ function draw() {
 // DISPLAY ////////////////////////////////////////////////////////////////////
 // Displays the player, tree, stump, sun
 function display(){
-//Display player
-  push();
-  fill(255);
-  ellipse(player.x, player.y, player.size);
-  pop();
-
 
 //Display tree
   push();
   fill(83,53,10);
   rectMode(CENTER);
   rect(tree.x, tree.y, tree.width, tree.height);
+  pop();
+
+//Display max
+  push();
+  image(axe.image, axe.x, axe.y, axe.size, axe.size);
   pop();
 
 //Display stump
@@ -118,6 +159,35 @@ function display(){
   fill(255, 255, 217, 100);
   ellipse(sun.x, sun.y, sun.size)
   pop();
+
+//Display floor
+  floor.width = width;
+
+  push();
+  fill(11, 102, 35);
+  rectMode(CENTER);
+  rect(floor.x, floor.y, floor.width, floor.height);
+  pop();
+
+//Display tent
+  push();
+  imageMode(CENTER);
+  image(tent.image, tent.x, tent.y, tent.size, tent.size);
+  pop();
+
+//Display player
+  push();
+  fill(255);
+  image(player.image, player.x, player.y);
+  pop();
+
+//Display inventory
+  push();
+  fill(0, 0, 0, 100);
+  rect(inventory.x, inventory.y, inventory.width, inventory.height, 20);
+  image(inventory.image, inventory.imageX, inventory.imageY, inventory.imageSize, inventory.imageSize);
+
+
 }
 
 // PLAYER CONTROLS /////////////////////////////////////////////////////////////
