126144e63c44aa046c7a0fc415815abd65effa5e
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-10-09 20:14:26 -0500

    P1: Added variables
    Added variables and the canvas for the base. Variables will most likely change throughout the project.

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -1,14 +1,47 @@
 /**************************************************
-Project 1 : title
+Project 1 : "Something fire themeed"
 Daniel Cacatian
 
 Here is a description of this template p5 project.
 **************************************************/
 
+let player = {
+  x: undefined,
+  y: undefined,
+  size: 50,
+  speed: 2,
+  image: undefined,
+}
+
+let inventory = {
+  x: 0,
+  y: 0,
+  size: undefined,
+}
+
+let tree = {
+  x: 0,
+  y: 0,
+  size: 100,
+}
+
+let stump = {
+  x: 0,
+  y: 0,
+  size: 100,
+}
+
+function preload(){
+
+};
+
+
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+
+  createCanvas(1500, windowHeight);
 
 }
 
@@ -16,5 +49,7 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+
+  background(0);
 
 }
