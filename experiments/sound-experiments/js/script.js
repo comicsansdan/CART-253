b278d34ece0dcf74478abe91c3241c545bb2223c
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-10-09 19:07:38 -0500

    Learned about sound and strict

--- experiments/sound-experiments/js/script.js
+++ experiments/sound-experiments/js/script.js
@@ -5,16 +5,49 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 
+"use strict";
+
+let circle = {
+  x:0,
+  y:0,
+}
+
+let barkSFX;
+let music;
+
+function preload() {
+  barkSFX = loadSound(`assets/sounds/bark.wav`);
+  music = loadSound(`assets/sounds/bark.wav`);
+}
+
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+  createCanvas(500,500);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
 
+  circle.x = mouseX;
+  circle.y = mouseY;
+
+  ellipse(circle.x,circle.y,100);
 }
+
+// SOUND ////////////////////////////////////////////////////////////////////
+// function mousePressed(){
+//   if (!music.isPlaying()) {
+//     music.play();
+//   }
+// }
+//
+// function keyPressed(){
+//   if (!music.isPlaying()) {
+//     music.play();
+//   }
+// }
