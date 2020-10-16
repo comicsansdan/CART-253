1b31ea827c7a77f7147ed59243d0c5901c987116
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-10-16 16:25:43 -0500

    E4: Displayed school of fishies
    Displayed the fishes using a for loop with the help of an array

--- exercises/exercise4/js/script.js
+++ exercises/exercise4/js/script.js
@@ -15,16 +15,48 @@ let user = {
 let school = [];
 let schoolSize = 10;
 
-// setup()
+//SETUP///////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(600, 600);
+
+  for (let i = 0; i < schoolSize; i++) {
+    school[i] = createFish(random(0, width), random(0, height));
+  }
 
 }
 
-// draw()
+// Creates a new JavaScript Object describing a fish and returns it
+function createFish(x, y) {
+  let fish = {
+    x: x,
+    y: y,
+    size: 50,
+    vx: 0,
+    vy: 0,
+    speed: 2
+  };
+  return fish;
+}
+
+//DRAW///////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 // Description of draw() goes here.
 function draw() {
+  background(12,164,255);
 
+    for (let i = 0; i < school.length; i++){
+    displayFish(school[i]);
+  }
+}
+
+//DISPLAY FUNCTIONS///////////////////////////////////////////////////////////////////////////////////////////////////////
+// Displays the provided fish on the canvas
+function displayFish(fish) {
+  push();
+  fill(225,192,39);
+  noStroke();
+  ellipse(fish.x, fish.y, fish.size);
+  pop();
 }
