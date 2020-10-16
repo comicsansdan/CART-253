6047409cec00dcd1e3b6340f7730344a6eb28da1
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-10-16 00:07:53 -0500

    Learned about arrays

--- experiments/arrays-experiments/js/script.js
+++ experiments/arrays-experiments/js/script.js
@@ -7,6 +7,7 @@ Here is a description of this template p5 project.
 
 "use strict";
 
+//INTERMEDIATE FUNCTIONS/////////////////////////////////////////////////////////////////////////////////////////
 // Our user, to move with the mouse
 let user = {
   x: 0,
@@ -21,94 +22,163 @@ let food3;
 let food4;
 let food5;
 
+//ARRAYS//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let school = [];
+let schoolSize = 10;
 
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(700, 700);
 
-  food1 = createFood(250,windowHeight/2);
-  food2 = createFood(350,windowHeight/2);
-  food3 = createFood(450,windowHeight/2);
-  food4 = createFood(550,windowHeight/2);
-  food5 = createFood(650,windowHeight/2);
+  //ARRAYS//////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Create four fish, positioned randomly
+                  //number of fish (4)
+  for (let i = 0; i < schoolSize; i++) {
+    school[i] = createFish(random(0, width), random(0, height));
+  }
+
+  //INTERMEDIATE FUNCTIONS/////////////////////////////////////////////////////////////////////////////////////////
+  // food1 = createFood(250,windowHeight/2);
+  // food2 = createFood(350,windowHeight/2);
+  // food3 = createFood(450,windowHeight/2);
+  // food4 = createFood(550,windowHeight/2);
+  // food5 = createFood(650,windowHeight/2);
 
 }
 
-function createFood(x,y){
-  let food = {
+// function createFood(x,y){
+//   let food = {
+//     x: x,
+//     y: y,
+//     size: 50,
+//     eaten: false
+//   };
+//   return food;
+// }
+
+//ARRAYS//////////////////////////////////////////////////////////////////////////////////////////////////////
+// createFish(x,y)
+// Creates a new JavaScript Object describing a fish and returns it
+function createFish(x, y) {
+  let fish = {
     x: x,
     y: y,
     size: 50,
-    eaten: false
+    vx: 0,
+    vy: 0,
+    speed: 2
   };
-  return food;
+  return fish;
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-
-//INTERMEDIATE FUNCTIONS/////////////////////////////////////////////////////////////////////////////////////////
   background(0);
 
-  // Move the user (with the mouse)
-  moveUser();
-
-  // Check whether the user has eaten either food
-  checkFood(food1);
-  checkFood(food2);
-  checkFood(food3);
-  checkFood(food4);
-  checkFood(food5);
-
-
-
-  // Display the user and foods
-  displayUser();
-  displayFood(food1);
-  displayFood(food2);
-  displayFood(food3);
-  displayFood(food4);
-  displayFood(food5);
-
-
+  //ARRAYS//////////////////////////////////////////////////////////////////////////////////////////////////////
+          //length property (stays up to date with the set number where the first array comes up)
+  for (let i = 0; i < school.length; i++){
+  moveFish(school[i]);
+  displayFish(school[i]);
 }
 
 //INTERMEDIATE FUNCTIONS/////////////////////////////////////////////////////////////////////////////////////////
-// Sets the user position to the mouse position
-function moveUser() {
-  user.x = mouseX;
-  user.y = mouseY;
+  // // Move the user (with the mouse)
+  // moveUser();
+  //
+  // // Check whether the user has eaten either food
+  // checkFood(food1);
+  // checkFood(food2);
+  // checkFood(food3);
+  // checkFood(food4);
+  // checkFood(food5);
+  //
+  //
+  //
+  // // Display the user and foods
+  // displayUser();
+  // displayFood(food1);
+  // displayFood(food2);
+  // displayFood(food3);
+  // displayFood(food4);
+  // displayFood(food5);
+
 }
 
-// Checks if the user overlaps the food1 object and eats it if so
-function checkFood(food) {
-  // We only want to check for an overlap if food1 hasn't been eaten yet
-  if (!food.eaten) {
-    let d = dist(user.x, user.y, food.x, food.y);
-    if (d < user.size / 2 + food.size / 2) {
-      food.eaten = true;
-    }
+//ARRAYS//////////////////////////////////////////////////////////////////////////////////////////////////////
+// moveFish(fish)
+// Chooses whether the provided fish changes direction and moves it
+function moveFish(fish) {
+  // Choose whether to change direction
+  let change = random(0, 1);
+  if (change < 0.05) {
+    fish.vx = random(-fish.speed, fish.speed);
+    fish.vy = random(-fish.speed, fish.speed);
   }
+
+  // Move the fish
+  fish.x = fish.x + fish.vx;
+  fish.y = fish.y + fish.vy;
+
+  // Constrain the fish to the canvas
+  fish.x = constrain(fish.x, 0, width);
+  fish.y = constrain(fish.y, 0, height);
 }
 
-// Draw the user as a circle
-function displayUser() {
+// displayFish(fish)
+// Displays the provided fish on the canvas
+function displayFish(fish) {
   push();
-  fill(255);
-  ellipse(user.x, user.y, user.size);
+  fill(200, 100, 100);
+  noStroke();
+  ellipse(fish.x, fish.y, fish.size);
   pop();
 }
 
-function displayFood(food){
-  if (!food.eaten) {
-    push();
-    fill(255,100,100);
-    ellipse(food.x, food.y, food.size);
-    pop();
-  }
+function mousePressed(){
+  let fish = createFish(mouseX, mouseY);
+  //Push adds it to the array (the school[i])
+  school.push(fish);
 }
+
+
+//INTERMEDIATE FUNCTIONS/////////////////////////////////////////////////////////////////////////////////////////
+// Sets the user position to the mouse position
+// function moveUser() {
+//   user.x = mouseX;
+//   user.y = mouseY;
+// }
+//
+// // Checks if the user overlaps the food1 object and eats it if so
+// function checkFood(food) {
+//   // We only want to check for an overlap if food1 hasn't been eaten yet
+//   if (!food.eaten) {
+//     let d = dist(user.x, user.y, food.x, food.y);
+//     if (d < user.size / 2 + food.size / 2) {
+//       food.eaten = true;
+//     }
+//   }
+// }
+//
+// // Draw the user as a circle
+// function displayUser() {
+//   push();
+//   fill(255);
+//   ellipse(user.x, user.y, user.size);
+//   pop();
+// }
+//
+// function displayFood(food){
+//   if (!food.eaten) {
+//     push();
+//     fill(255,100,100);
+//     ellipse(food.x, food.y, food.size);
+//     pop();
+//   }
+// }
