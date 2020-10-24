57359b1b16c884bf428dcfa873e7bfa6b198c35b
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-10-24 18:26:16 -0500

    Activity 7: Completed

--- activities/07-object-orientated-programming-activity/js/script.js
+++ activities/07-object-orientated-programming-activity/js/script.js
@@ -1,20 +1,51 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Activity 07: Juggling Garden
+Daniel Cacatian
 
-Here is a description of this template p5 project.
+Creating a juggling simulation with a gravity simulation using object-orientated-programming
 **************************************************/
+
+"use strict"
+
+let gravityForce = 0.0025;
+
+let paddle;
+
+let balls = [];
+let numBalls = 10;
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  paddle = new Paddle(300, 20);
+
+  for (let i = 0; i < numBalls; i++) {
+    let x = random(0, width);
+    let y = random(-400, -100);
+    let ball = new Ball(x, y);
+    balls.push(ball);
+  }
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
 
+  paddle.move();
+  paddle.display();
+
+  for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    if (ball.active){
+    ball.gravity(gravityForce);
+    ball.move();
+    ball.bounce(paddle);
+    ball.display();
+    }
+  }
 }
