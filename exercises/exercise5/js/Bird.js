6c88c40a28d345f51f70e15109f16c075be9c237
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-10-25 18:53:57 -0500

    E5: Added new class: Bird.js
    The bird is what will kill the player if he/she gets eaten by it then it is game over.

---
+++ exercises/exercise5/js/Bird.js
@@ -0,0 +1,56 @@
+class Bird {
+
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 60;
+    this.vx = 0;
+    this.vy = 0;
+    this.speed = 1;
+    this.maxSpeed = 5;
+    this.jitteriness = 0.2; // How likely the bird is to change direction
+  }
+
+  move() {
+    // First check if we should change direction
+    let r = random(0, 1);
+    if (r < this.jitteriness) {
+      this.vx = random(-this.speed, this.speed);
+      this.vy = random(-this.speed, this.speed);
+    }
+
+    // Update position with velocity to actually move
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy;
+
+    // Constrain to the canvas (guess it's a walled garden!)
+    this.x = constrain(this.x, 0, width);
+    this.y = constrain(this.y, 0, height);
+  }
+
+  // display() draws our bird onto the canvas
+  display() {
+    push();
+    // Wings on either side
+    fill(207, 251, 238);
+    noStroke();
+    ellipse(this.x - this.size / 2, this.y, this.size / 2);
+    ellipse(this.x + this.size / 2, this.y, this.size / 2);
+    pop();
+
+    // Body
+    push();
+    fill(207, 251, 238);
+    noStroke();
+    ellipse(this.x, this.y, this.size);
+    pop();
+
+    // Eyes
+    push();
+    fill(0, 0, 0);
+    noStroke();
+    ellipse(this.x - this.size / 10, this.y, this.size / 10);
+    ellipse(this.x + this.size / 10, this.y, this.size / 10);
+    pop();
+  }
+}
