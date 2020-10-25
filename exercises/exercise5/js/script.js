ecbd76f9fe3cb4fc4c2717c74edfb5d8f5a10998
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-10-25 16:54:09 -0500

    E5: Displayed the flowers
    Display the Flower class in the script.js. Kept the properties from the experiment so they continiously shrink.

--- exercises/exercise5/js/script.js
+++ exercises/exercise5/js/script.js
@@ -2,13 +2,51 @@
 Exercise 5: Juggling Garden
 Daniel Cacatian
 
-An exercise using OOP where I'll be reusing the code from the flower simulation and add my own code. 
+An exercise using OOP where I'll be reusing the code from the flower simulation and add my own code.
 **************************************************/
+
+"use strict";
+
+// Our garden
+let garden = {
+  // An array to store the individual flowers
+  flowers: [],
+  // How many flowers in the garden
+  numFlowers: 20,
+  // An array of bees
+  bees: [],
+  // How many bees in the garden
+  numBees: 7,
+  // The color of the grass (background)
+  grassColor: {
+    r: 120,
+    g: 180,
+    b: 120
+  }
+};
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(600,600);
+
+  // Create our flowers by counting up to the number of the flowers
+  for (let i = 0; i < garden.numFlowers; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    let size = random(50,80);
+    let stemLength = random(50, 100);
+    let petalColor = {
+      r: random(100,255),
+      g: random(100,255),
+      b: random(100,255),
+    }
+
+    let flower = new Flower(x, y, size, stemLength, petalColor);
+    // Add the flower to the array of flowers
+    garden.flowers.push(flower);
+  }
 
 }
 
@@ -16,5 +54,17 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+  // Display the grass
+  background(garden.grassColor.r, garden.grassColor.g, garden.grassColor.b);
 
+  // Loop through all the flowers in the array and display them
+  for (let i = 0; i < garden.flowers.length; i++) {
+    let flower = garden.flowers[i];
+
+  //Will only display flowers that are alive
+    if (flower.alive) {
+    flower.shrink();
+    flower.display();
+    }
+  }
 }
