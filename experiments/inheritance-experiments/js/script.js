5383d5f73132d529d803748e9e8889a888dea0dc
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-10-31 20:16:07 -0500

    Learned polymorphism

--- experiments/inheritance-experiments/js/script.js
+++ experiments/inheritance-experiments/js/script.js
@@ -7,11 +7,14 @@ Here is a description of this template p5 project.
 
 "use strict"
 
+// POLYMORPHISM ////////////////////////////////////////////////////////////////
+let vehicles = [];
+
 // INHERITANCE ////////////////////////////////////////////////////////////////
-let cars = [];
+// let cars = [];
 let numCars = 10;
 
-let motorcycles = [];
+// let motorcycles = [];
 let numMotorcycles = 10;
 
 // setup()
@@ -25,14 +28,18 @@ function setup() {
     let x = random(0, width);
     let y = random(0, height);
     let car = new Car(x,y);
-    cars.push(car);
+    // cars.push(car);
+// POLYMORPHISM ////////////////////////////////////////////////////////////////
+    vehicles.push(car);
   }
 
   for (let i = 0; i < numMotorcycles; i++) {
     let x = random(0, width);
     let y = random(0, height);
     let motorcycle = new Motorcycle(x,y);
-    motorcycles.push(motorcycle);
+    // motorcycles.push(motorcycle);
+// POLYMORPHISM ////////////////////////////////////////////////////////////////
+    vehicles.push(motorcycle);
   }
 }
 
@@ -42,18 +49,26 @@ function setup() {
 function draw() {
   background(0);
 
-// INHERITANCE ////////////////////////////////////////////////////////////////
-  for (let i = 0; i < cars.length; i++) {
-    let car = cars[i];
-    car.move();
-    car.wrap();
-    car.display();
+// POLYMORPHISM ////////////////////////////////////////////////////////////////
+  for (let i = 0; i < vehicles.length; i++) {
+    let vehicle = vehicles[i];
+    vehicle.move();
+    vehicle.wrap();
+    vehicle.display();
   }
 
-  for (let i = 0; i < motorcycles.length; i++) {
-    let motorcycle = motorcycles[i];
-    motorcycle.move();
-    motorcycle.wrap();
-    motorcycle.display();
-  }
+// INHERITANCE ////////////////////////////////////////////////////////////////
+  // for (let i = 0; i < cars.length; i++) {
+  //   let car = cars[i];
+  //   car.move();
+  //   car.wrap();
+  //   car.display();
+  // }
+  //
+  // for (let i = 0; i < motorcycles.length; i++) {
+  //   let motorcycle = motorcycles[i];
+  //   motorcycle.move();
+  //   motorcycle.wrap();
+  //   motorcycle.display();
+  // }
 }
