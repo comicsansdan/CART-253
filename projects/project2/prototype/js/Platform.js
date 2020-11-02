35d561f63f5221a493bcdb06e3d3ea45b88a677e
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-11-02 02:06:35 -0600

    Prototype: Modified Platform.js

--- projects/project2/prototype/js/Platform.js
+++ projects/project2/prototype/js/Platform.js
@@ -1,15 +1,15 @@
 class Platform {
 
   constructor(x, y){
-    this.x = x;
-    this.y = y;
+    this.x = 600;
+    this.y = 300;
     this.width = 200;
     this.height = 40;
   }
 
   display() {
     push();
-    fill(255, 255, 0);
+    fill(255, 120, 0);
     noStroke();
     rectMode(CENTER);
     rect(this.x, this.y, this.width, this.height);
