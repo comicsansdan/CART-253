06471058d888853a94e2f776a089d1ced8221f67
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-11-02 17:21:08 -0600

    Prototype: Started prototype

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -1,33 +1,14 @@
 /**************************************************
-Project 2 Prototype
+Template p5 project
 Pippin Barr
 
 Here is a description of this template p5 project.
 **************************************************/
 
-let gravityForce = 0.0025;
-
-let player;
-
-let platform;
-
-let obstacle;
-
-let ground;
-
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-  createCanvas(800, 500);
-
-  player = new Player(width/2, height-25);
-
-  platform = new Platform(width/2, height, 600, 25);
-
-  let x = width/2;
-  let w = width;
-  ground = new Ground(x, w);
 
 }
 
@@ -36,20 +17,4 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
 
-  background(150, 230, 240);
-
-  //Display ground
-  ground.display();
-
-  let canvasHeight = height - 40;
-
-  platform.display();
-
-  player.handleInput();
-  player.gravity(gravityForce);
-  player.wrap(platform);
-  player.move();
-  player.display();
-  player.jumpCooldown(platform);
-
-  }
+}
