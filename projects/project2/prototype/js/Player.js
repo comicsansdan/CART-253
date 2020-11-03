1db9568652dfae077f7d61572753cfa71626a404
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-11-02 19:55:46 -0600

    Prototype: Organized code
    Organized the Player.js so i can reuse and modify for the actual project 2

--- projects/project2/prototype/js/Player.js
+++ projects/project2/prototype/js/Player.js
@@ -39,20 +39,20 @@ class Player {
       this.jumpCounter = 0; //Resets jump counter (limits the amount of time in the air you can jump)
     }
     else {
-      this.y += this.vy*force; //Falling
+      this.y += this.vy*force; //Falling (gravity)
     }
 
     if (this.jump){
-      if(this.y <= 50 || this.jumpCounter >= this.jumpPower )
+      if(this.y <= 50 || this.jumpCounter >= this.jumpPower )// Reached max jump height/jumpPower
         if(this.y >= 375){
-          this.y = 375;
+          this.y = 375; //Stays at the minimum height
         }
         else {
         this.vy = this.speed;
       }
       else {
-      this.vy = -this.jumpPower; //Jumping
-      this.jumpCounter += 1
+      this.vy = -this.jumpPower; //Jumping (only once)
+      this.jumpCounter += 1 //Add to jump counter
       }
     }
     else {
