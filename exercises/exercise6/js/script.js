2a5aaf5042978c0cb203a0745bc78af07fb81499
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-11-08 23:19:25 -0600

    Exercise 6: Started exercise 6

---
+++ exercises/exercise6/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Exercise 06: Make Some Noise
+Daniel Cacatian
+
+This project use sound in form of a puzzle. Play the tune in the correct order and you win
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
