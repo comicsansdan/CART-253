988c05b1907515147b83873b821051b528b3fe94
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-11-09 00:03:55 -0600

    E6: Added Synth variables
    Added the variables for the polysynth, which will play the tune the player has to recreate

--- exercises/exercise6/js/script.js
+++ exercises/exercise6/js/script.js
@@ -5,10 +5,16 @@ Daniel Cacatian
 This project use sound in form of a puzzle. Play the tune in the correct order and you win
 **************************************************/
 
+//Song
+let song; //The tune the player must recreate
+let notes = [`B4`, `A4`, `F4`, `B4`, `A4`, `F4`, `A4`, `F4`, `E4`, `D4`, `E4` ];
+let currentNote = 0;
+
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(800, 800);
 
 }
 
@@ -16,5 +22,6 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
 
 }
