15a4b4819f9bcdb5fafd860cb57729c26d2f8836
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2020-12-01 18:34:15 -0600

    P2: Fixed sound of platforms
    When you stood on a platform, it would continiously play the note, but I fixed it and made it so that it stops playing after 1 second.

--- projects/project2/js/Platforms/Platform.js
+++ projects/project2/js/Platforms/Platform.js
@@ -8,6 +8,8 @@ class Platform{
     this.vx = 2;
     this.speed = 2;
     this.moving = false;
+    this.noteTimer = 0;
+    this.noteLength = 60 * 1; //2 secondes
 
     //Synth
     this.note = undefined;
@@ -19,19 +21,33 @@ class Platform{
   }
 
   pressedOn(player){
+    console.log(this.noteTimer);
     //Player colides with platform
     if(player.x >= this.x-this.width/2 &&
       player.x <= this.x+this.width/2 &&
       player.y+player.size/2 >= this.y-this.height/2 &&
-      player.y+player.size/2 <= this.y+this.height/2){
+      player.y+player.size/2 <= this.y+this.height/2 &&
+      this.noteTimer <= this.noteLength){
 
       this.synth.play(this.note, 0.25, 0, 0.3);
+
+      this.speed = 0;
+      this.moving = false;
+
+      this.noteTimer++; //Timer goes up
+    }
+    else if(player.x >= this.x-this.width/2 &&
+    player.x <= this.x+this.width/2 &&
+    player.y+player.size/2 >= this.y-this.height/2 &&
+    player.y+player.size/2 <= this.y+this.height/2 &&
+    this.noteTimer >= this.noteLength){
 
       this.speed = 0;
       this.moving = false;
     }
     else{
       this.speed = 2;
+      this.noteTimer = 0;//Reset timer
     }
   }
 
